aca27d9993007bf66f4c9f41fe3c35f328d695ee
Arslan-95 <arslan180198@gmail.com>
2023-06-01 00:30:05 +0300

    [feature/swap] Fix AmountInput

--- src/components/dapp/AmountInput.tsx
+++ src/components/dapp/AmountInput.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Input } from '@components/ui';
 import { InputProps, InputValue } from '@components/ui/types';
+import _ from 'lodash';
 
 type Props = {
   value: number | undefined;
-  onChange: (value: number) => void;
+  onChange: (value: number | undefined) => void;
 };
 
 const AmountInput = ({
@@ -12,13 +13,20 @@ const AmountInput = ({
   onChange,
   ...otherProps
 }: Props | InputProps) => {
-  const [inputValue, setInputValue] = React.useState<InputValue>(
-    String(value || '')
-  );
+  const valueInString = _.isNumber(value) ? String(value) : '';
+  const [inputValue, setInputValue] = React.useState<InputValue>(valueInString);
 
   React.useEffect(() => {
-    onChange(Number(inputValue));
+    if (valueInString === inputValue) return;
+
+    onChange(inputValue ? Number(inputValue) : undefined);
   }, [inputValue]);
+
+  React.useEffect(() => {
+    if (valueInString === inputValue) return;
+
+    setInputValue(valueInString);
+  }, [value]);
 
   return (
     <Input
