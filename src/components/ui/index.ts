0174025429f166f1c2c42800734e5682ab4e7c6a
Arslan-95 <arslan180198@gmail.com>
2023-06-06 21:33:28 +0300

    [feature/swap] UI - CustomButton is added

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Box } from './Box';
 export { default as Container } from './Container';
 export { default as Input } from './Input';
+export { default as CustomButton } from './CustomButton';
