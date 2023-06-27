c1e7bdc72f6bfee63b3b54b6dab2ae2822957417
Arslan-95 <arslan180198@gmail.com>
2023-06-27 04:48:06 +0300

    [feature/swap] Add redirects to the Swap page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Swap from '@pages/Swap';
 import * as PAGES from '@pages/PAGES';
 import Theme from '@styles/Theme';
 import Dapp from '@layouts/Dapp';
 import useAdaptive from '@features/adaptive/useAdaptive';
 
-const App: React.FC = () => {
+const App = () => {
   useAdaptive();
 
   return (
     <BrowserRouter>
       <Theme>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <Link to={PAGES.SWAP}>SWAP</Link>
-              </div>
-            }
-          />
+          <Route path="*" element={<Navigate to={PAGES.SWAP} replace />} />
           <Route path={PAGES.DAPP} element={<Dapp />}>
             <Route path={PAGES.SWAP} element={<Swap />} />
+            <Route
+              path={PAGES.DAPP}
+              element={<Navigate to={PAGES.SWAP} replace />}
+            />
           </Route>
         </Routes>
       </Theme>
